f110e4784901538f3c626e4e97bfe73f94e3c7eb
Facundo Andrioli Villa <facundoo30@gmail.com>
2025-09-30 16:54:46 -0300

    agregar datos

--- django/aprendiendo/myapp/models.py
+++ django/aprendiendo/myapp/models.py
@@ -2,5 +2,15 @@ from django.db import models
 
 # Create your models here.
 
-class MiModelo(models.Model):
-    name = models.CharField(max_length=100)
+class Sensor(models.Model):
+    nombre = models.CharField(max_length=100)
+    valor = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.nombre} ({self.valor})"
+
+class Lectura(models.Model):
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE)
+    valor = models.FloatField()
+    fecha = models.DateTimeField(auto_now_add=True)
+
